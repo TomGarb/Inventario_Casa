bb6e091a02869d7f2490d5c2fc123002d8b61977
TomGarb <tgarbossa@frba.utn.edu.ar>
2026-07-30 20:08:57 -0300

    Mejora de importacion

--- models/database.py
+++ models/database.py
@@ -1,6 +1,7 @@
 from extensions import db, login_manager
 from flask_login import UserMixin
 from datetime import datetime, date
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class Usuario(UserMixin, db.Model):
     __tablename__ = 'usuarios'
